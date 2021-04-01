6507a364ebe153944a662588cbbf4597de1685e8
Kevin Ewing <kevin@ewing.io>
2021-03-31 20:39:15 -0600

    clean up todos in dissmissFeedbackSolicitation

--- subtasks/dismissFeedbackSolicitation.js
+++ subtasks/dismissFeedbackSolicitation.js
@@ -1,13 +1,10 @@
 module.exports = async (page, CONFIG) => {
   try {
     await page.$eval(CONFIG.FEEDBACK_SOLICITATION_CLOSE_BUTTON_SELECTOR, async $closeSolicitationButton => { 
-      if ($closeSolicitationButton) {
-        await page.screenshot({ path: `${pageTitle}-pre.png` }); // @TODO Only here for testing
-        await $closeSolicitationButton.click()
-        await page.screenshot({ path: `${pageTitle}-post.png` }); // @TODO Only here for testing
-      }
+      if ($closeSolicitationButton) await $closeSolicitationButton.click()
     });
   } catch (e) {
+    // This space intentionally left empty...
     // we want to silence the error thrown if the solicitation is not found
   }
 };
